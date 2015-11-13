6b86d81278fa2c462c1d1e796423b31b58193e68
Patrick Andersen <patrick@bacha.dk>
2015-11-13 10:49:23 +0100

    get min and max time in

--- etl.py
+++ etl.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 
 import getpass
-# import datetime
-# import sys
-# import time
+import datetime
+import calendar
+import sys
+import time
 import psycopg2  # postgresql
 
 import pygrametl
@@ -40,8 +41,7 @@ src_sem_group = LoadDataSource('SemesterGroups.txt')
 """
 pgconn_rw = psycopg2.connect(user=username, database=db_rw)
 conn_rw = pygrametl.ConnectionWrapper(pgconn_rw)
-conn_rw.execute('set search_path to pygrametlexa')
-"""
+conn_rw.execute('set search_path to pygrametlexa') """
 
 # pgconn_dw = psycopg2.connect("user='"+username+"' dbname='"+db_dw+"'")
 pgconn_dw = psycopg2.connect(user=username, database=db_dw)
@@ -51,9 +51,7 @@ conn_dw.setasdefault()
 
 # TRUNCATE
 
-for t in tables_names:
-    conn_dw.execute("TRUNCATE TABLE "+t+" CASCADE;")
-
+[conn_dw.execute("TRUNCATE TABLE "+t+" CASCADE;") for t in tables_names]
 
 # ROW EXPANDERS
 
@@ -97,6 +95,37 @@ fact_sale = FactTable(
 # PROGRAM
 
 
+def GetMinMaxDate(rounded=True):
+    min_s = '1996-10-28 12:21:23'
+    max_s = '2008-01-07 12:19:39'
+    fmt = '%Y-%m-%d %H:%M:%S'
+    min_t = time.strptime(min_s, fmt)
+    max_t = time.strptime(max_s, fmt)
+    if(rounded):
+        # min_f = time.mktime(min_t)
+        # max_f = time.mktime(max_t)
+        min_f = calendar.timegm(min_t)
+        max_f = calendar.timegm(max_t)
+        qrt_f = 15*60.0  # 15 minutes
+        min_f -= min_f % qrt_f
+        max_f += qrt_f  # round up instead of down
+        max_f -= max_f % qrt_f
+        min_t = time.gmtime(min_f)
+        max_t = time.gmtime(max_f)
+    return min_t, max_t
+
+
+def TimeToRow(dt):
+    """
+           c <
+          /\/    o    o    o    o
+         /_    /\/) /\/) /\/) /\/)
+       _[]/___/__/_/__/_/__/_/__/______
+    -~-~ `---/----/----/----/-------'  -~-~
+    -~-~-~-~  -~-~  -~-~-~-~
+    """
+
+
 def TimeGenerator():
     raise "meh"
 
@@ -107,6 +136,12 @@ def TimeGenerator():
 
 def main():
     print("main")
+    min_d, max_d = GetMinMaxDate(False)
+    print (min_d)
+    print (max_d)
+    min_r, max_r = GetMinMaxDate()
+    print (min_r)
+    print (max_r)
 
 
 if __name__ == '__main__':
